ecffe962e1605d438cfa719f8601f8a381c0d081
Aris Barlos <aris13pat@hotmail.com>
2025-05-25 16:37:42 +0300

    fix for correctly fetching reservations for a barber

--- sql/getReservations/getReservations.sql
+++ sql/getReservations/getReservations.sql
@@ -1,7 +1,13 @@
+/*
+ * Accepts a shop_id, some_date, barber_id and retrieves the opening_time and closing_time
+ * @param shop_id - The id of the shop to retrieve the opening_time and closing_time
+ * @param barber_id - The barber_id that we will retrieve his reservations
+ * @param some_date - The date to extract the working hours for the month 
+ */
 DROP PROCEDURE IF EXISTS getReservations;
 -- The Dates of the month
 DELIMITER $$
-CREATE PROCEDURE getReservations(IN shop_id INT, IN barberId INT,IN some_date DATE)
+CREATE PROCEDURE getReservations(IN shop_id INT, IN barber_id INT,IN some_date DATE)
 BEGIN
   -- Selects opening_time and closing_time for `shop_id`
   SELECT opening_time, closing_time 
@@ -12,7 +18,8 @@ BEGIN
   SELECT reservation_date AS r_date, reservation_time as r_time
   FROM reservations 
   WHERE YEAR(some_date) = YEAR(reservation_date)
-  AND MONTH(some_date) = MONTH(reservation_date);
+  AND MONTH(some_date) = MONTH(reservation_date)
+  AND reservations.barber_id = barber_id;
 END $$
 DELIMITER ;
 
